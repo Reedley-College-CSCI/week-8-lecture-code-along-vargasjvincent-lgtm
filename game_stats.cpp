991ee22de07ec51ec5430f12baeba0b1830c5785
Vincent Vargas RCPhy82 <0964572@MY.SCCCD.EDU>
2025-10-06 14:18:21 -0700

    Added static array demo and memory size output

--- game_stats.cpp
+++ game_stats.cpp
@@ -2,14 +2,30 @@
 // Oct 6 2025
 #include <iostream>
 using namespace std;
+
+void showDrive();
 int main() {
-    const int SIZE = 10
-    cout << "Detroit Lions at Baltimore Ravens" << endl
-    << "Sept 29, 2025 Game Statistics" << endl;
+    const int SIZE = 10;
+    cout << "Detroit Lions at Baltimore Ravens" << endl;
+    cout << "Sept 29, 2025 Game Statistics" << endl;
 
     int yards[SIZE] = { 67, 98, 60, 94, 35, 25, 45, 50, 30, 40 };
     cout << "First drive yards: " << yards[0] << endl;
-    cout << "Epic second drive" << yards[1] << " yards" << endl;
+    cout << "Epic second drive: " << yards[1] << " yards" << endl;
+    showDrive();
+    showDrive();
+    // display memory usage of array
+
+    cout << "Array memory: #" << sizeof(yards) << " bytes" << endl;
+
+   
 
     return 0; 
+}
+
+void showDrive()
+{
+    static int driveNum = 1; // Persists across function calls
+    cout << "Showing drive " << driveNum << endl;
+    driveNum++
 }
