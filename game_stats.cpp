6d5b5799cffafad133e098bf73544372374cf492
Vincent Vargas RCPhy82 <0964572@MY.SCCCD.EDU>
2025-10-06 14:01:25 -0700

    Added array declarartion for Lions drive yards.

--- game_stats.cpp
+++ game_stats.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main() {
     cout << "Detroit Lions at Baltimore Ravens" << endl
     << "Sept 29, 2025 Game Statistics" << endl;
-    return 0;
+
+    int yards[10] = { 67, 98, 60, 94, 35, 25, 45, 50, 30, 40 };
+    cout << "First drive yards: " << yards[0] << endl;
+
+    return 0; 
 }
