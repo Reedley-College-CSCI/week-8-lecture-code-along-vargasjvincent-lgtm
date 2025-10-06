f47704f8503227bd35ac44449d8da70ab862924f
Vincent Vargas RCPhy82 <0964572@MY.SCCCD.EDU>
2025-10-06 14:06:19 -0700

    Added constant size and accessed second drive yards

--- game_stats.cpp
+++ game_stats.cpp
@@ -3,11 +3,13 @@
 #include <iostream>
 using namespace std;
 int main() {
+    const int SIZE = 10
     cout << "Detroit Lions at Baltimore Ravens" << endl
     << "Sept 29, 2025 Game Statistics" << endl;
 
-    int yards[10] = { 67, 98, 60, 94, 35, 25, 45, 50, 30, 40 };
+    int yards[SIZE] = { 67, 98, 60, 94, 35, 25, 45, 50, 30, 40 };
     cout << "First drive yards: " << yards[0] << endl;
+    cout << "Epic second drive" << yards[1] << " yards" << endl;
 
     return 0; 
 }
